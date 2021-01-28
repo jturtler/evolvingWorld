f940d734b9d3789beebbd26cd272b604058335b6
Edward J <edjeong99@gmail.com>
2021-01-27 22:32:11 -0600

    move js code to own js files

--- evolvingWorld/js/kid.js
+++ evolvingWorld/js/kid.js
@@ -1,5 +1,5 @@
-// --------------------------------------
-// -- Kids Class
+//  - Kids Class 		- Kids object...
+
 function Kid(stage, name, attribute, locationX, locationY) {
   var me = this;
 
@@ -286,4 +286,49 @@ function Kid(stage, name, attribute, locationX, locationY) {
   // -----------------------------------------
 
   me.initializeObj();
+}
+
+//	- KidsBuilder Class - Builder class for Kids Object
+
+function KidsBuilder(stageObj, canvasWidth, canvasHeight) {
+  var me = this;
+
+  me.stageObj = stageObj;
+  me.kidNo = 0;
+
+  me.createKids = function (kidsNum) {
+    for (var i = 0; i < kidsNum; i++) {
+      me.createKid();
+    }
+  };
+
+  me.createKid = function (name) {
+    if (!name) name = 'kid' + me.kidNo; // TODO: We can create kids with somewhat related to the attribute..
+
+    // TODO: Need to randomly give diff attributes to the kids...
+    var kidObj = me.createContainer(
+      Kid,
+      name,
+      Builder.newAttribute(),
+      Builder.newLocationXY(canvasWidth, canvasHeight)
+    );
+    me.stageObj.addChild(kidObj);
+    INFO.kids.push(kidObj);
+
+    me.kidNo++;
+
+    return kidObj;
+  };
+
+  // Generic 'Container' class (of createjs) - Could be something other than Kid class
+  me.createContainer = function (childClass, name, attribute, locationXY) {
+    childClass.prototype = new createjs.Container();
+    return new childClass(
+      me.stageObj,
+      name,
+      attribute,
+      locationXY.locX,
+      locationXY.locY
+    );
+  };
 }
